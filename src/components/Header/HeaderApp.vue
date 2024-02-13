a30af1319297158fb5657f576a07387549cfc657
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-02-13 12:17:31 +0100

    complated layout

---
+++ src/components/Header/HeaderApp.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+data() {
+    return {
+        name:"HeaderApp"
+    }
+},
+}
+
+</script>
+
+<template>
+ 
+<div>
+
+    <h1>BOOLFLIX</h1>
+    <input type="text" name="" id="">
+
+</div>
+
+</template>
+
+<style scoped lang="scss">
+@use "../../styles/partials/variables" as *;
+
+div{
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    padding: 20px;
+    background-color: black;
+        h1{
+            color: red;
+        }
+        input{
+            height: 30px;
+            width: 230px;
+        }
+}
+</style>
