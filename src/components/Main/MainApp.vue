77e67da144b6d4b51dd2ebd025d9ec5756ac19e8
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-02-14 14:45:56 +0100

    added horizontally scroll

--- src/components/Main/MainApp.vue
+++ src/components/Main/MainApp.vue
@@ -27,6 +27,20 @@ import axios from "axios"
                     console.log(store.listtv)
                 })
         },
+        methods: {
+            scrollX(e) {
+    if (this.$refs.scroll_container.contains(e.target)) {
+      this.$refs.scroll_container.scrollLeft += e.deltaY;
+    }
+  },
+
+  scrollXX(e) {
+    if (this.$refs.scroll_container2.contains(e.target)) {
+      this.$refs.scroll_container2.scrollLeft += e.deltaY;
+    }
+  },
+  
+},
     }
 </script>
 
@@ -37,13 +51,13 @@ import axios from "axios"
     <div class="container">
         <h3>MOVIE</h3>
 
-        <div class="containermovie">
+        <div class="containermovie" ref="scroll_container" @wheel.prevent="scrollX">
         <SingleCard v-for="(element, index) in store.list" :key="index" :data="element"/>
         </div>
 
         <h3>TV</h3>
 
-        <div class="containertv">
+        <div class="containertv" ref="scroll_container2" @wheel.prevent="scrollXX">
         <SingleTv v-for="(element, index) in store.listtv" :key="index" :data="element"/>
         </div>
     </div>
