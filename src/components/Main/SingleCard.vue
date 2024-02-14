1a392370ecf1cdf4b8e86e61dc4db80801ea2f69
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-02-14 14:09:28 +0100

    fix flag

--- src/components/Main/SingleCard.vue
+++ src/components/Main/SingleCard.vue
@@ -46,7 +46,7 @@ import { store } from "../../store"
     <div class="visible" v-show="upHere">
         <span>Titolo:<span>{{data.title}}</span></span>
         <span v-show="data.title!=data.original_title">Titolo originale:<span>{{ data.original_title }}</span></span>
-        <span class="spancenter">Lingua: <img class="flag" v-if="country !== `EN` " :src="`https://flagsapi.com/${country}/shiny/24.png`"> <img class="flag" v-else src="https://img.freepik.com/free-vector/illustration-uk-flag_53876-18166.jpg?size=626&ext=jpg&ga=GA1.1.1141335507.1707436800&semt=ais" alt=""></span>
+        <span class="spancenter">Lingua: <span :class="`lang-icon lang-icon-${data.original_language}`"></span></span>
         <span>Voto:</span> <div class="star"><span v-for="element in vote"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg></span> <span v-for="element in reststar"><svg style="opacity: 0.5;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg></span></div>
         <span>
             <p>{{ data.overview }}</p>
