457eda60860caa55ade168af80a9f8c1645bacac
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-02-15 11:32:07 +0100

    modified style for text in card hover

--- src/components/Main/SingleCard.vue
+++ src/components/Main/SingleCard.vue
@@ -81,16 +81,21 @@ import { store } from "../../store"
         <!-- uyM1tesNMM7fg6zmozn8MYD036G.jpg -->
         
         <div class="visible" v-show="upHere">
-            <span>Titolo:<span>{{data.title}}</span></span>
-            <span v-show="data.title!=data.original_title">Titolo originale:<span>{{ data.original_title }}</span></span>
-            <span class="spancenter">Lingua: <span :class="`lang-icon lang-icon-${data.original_language}`"></span></span>
-            <span>Voto:</span> <div class="star"><span v-for="element in vote"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg></span> <span v-for="element in reststar"><svg style="opacity: 0.5;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg></span></div>
-            <span>
-                <p>{{ data.overview }}</p>
-            </span>
-            <span v-for="element in actor">
-            <span> {{ element.original_name}} </span> 
-            </span>
+            <span><span class="bold">Titolo:</span><span>{{data.title}}</span></span>
+            <span v-show="data.title!=data.original_title"> <span class="bold">Titolo originale:</span> <span>{{ data.original_title }}</span></span>
+            <span class="spancenter"><span class="bold">Lingua</span> <span :class="`lang-icon lang-icon-${data.original_language}`"></span></span>
+            <div class="star"><span><span class="bold">Voto:</span></span><span v-for="element in vote"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg></span> <span v-for="element in reststar"><svg style="opacity: 0.5;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg></span></div>
+            
+                
+                <p><span class="bold">Descrizione:</span> {{ data.overview }}</p>
+
+            
+            <div class="act">
+                <span class="bold">Attori:</span>
+                <span v-for="element in actor">
+                    {{ element.original_name}} &CenterDot;
+                </span>
+            </div>
             
           
             </div>
@@ -101,6 +106,8 @@ import { store } from "../../store"
 
 
 <style lang="scss" scoped>
+@use "../../styles/partials/mixins" as *;
+    
     
     svg{
         width: 15px;
@@ -113,6 +120,7 @@ import { store } from "../../store"
     }
     div{
         min-width: 350px;
+        max-width: 350px;
         position: relative;
         
             figure{
@@ -141,10 +149,19 @@ import { store } from "../../store"
                 background-color: rgba(128, 128, 128, 0.877);
                 padding: 10px;
                 justify-content: center;
-                gap: 10px;
-                font-weight: bold;
+                gap: 7px;
+
+                .bold{
+                    font-weight: bold;
+                }
+    
+                p{
+                    @include text-preview
+                }
+                
                 .star{
                     display: flex;
+                    gap: 2px;
                     
                 }
                 .flag{
--- src/components/Main/SingleTv.vue
+++ src/components/Main/SingleTv.vue
@@ -78,17 +78,22 @@ import axios from "axios"
         </figure>
         <!-- uyM1tesNMM7fg6zmozn8MYD036G.jpg -->
         
-    <div class="visible" v-show="upHere">
-        <span>Titolo:<span>{{data.name}}</span></span>
-        <span v-show="data.name!=data.original_name">Titolo originale:<span>{{ data.original_name }}</span></span>
-        <span class="spancenter">Lingua: <span :class="`lang-icon lang-icon-${data.original_language}`"></span></span>
-        <span>Voto:</span> <div class="star"><span v-for="element in vote"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg></span> <span v-for="element in reststar"><svg style="opacity: 0.5;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg></span></div>
-        <span>
-            <p>{{ data.overview }}</p>
-        </span>
-        <span v-for="element in actor">
-            <span> {{ element.original_name}} </span> 
-            </span>
+        <div class="visible" v-show="upHere">
+            <span><span class="bold">Titolo:</span><span>{{data.name}}</span></span>
+            <span v-show="data.name!=data.original_name"><span class="bold">Titolo originale:</span><span>{{ data.original_name }}</span></span>
+            <span class="spancenter"><span class="bold">Lingua</span> <span :class="`lang-icon lang-icon-${data.original_language}`"></span></span>
+            <div class="star"><span><span class="bold">Voto:</span></span> <span v-for="element in vote"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg></span> <span v-for="element in reststar"><svg style="opacity: 0.5;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg></span></div>
+        
+            <p><span class="bold">Descrizione:</span>{{ data.overview }}</p>
+            
+            <div class="act">
+                <span class="bold">Attori:</span>
+                <span v-for="element in actor">
+                <span> 
+                    {{ element.original_name}} &CenterDot; 
+                </span> 
+                </span>
+            </div>
         </div>
     </div>
 </template>
@@ -97,6 +102,8 @@ import axios from "axios"
 
 
 <style lang="scss" scoped>
+    @use "../../styles/partials/mixins" as *;
+    
     
     svg{
         width: 15px;
@@ -109,6 +116,7 @@ import axios from "axios"
     }
     div{
         min-width: 350px;
+        max-width: 350px;
         position: relative;
         
             figure{
@@ -137,10 +145,19 @@ import axios from "axios"
                 background-color: rgba(128, 128, 128, 0.877);
                 padding: 10px;
                 justify-content: center;
-                gap: 10px;
-                font-weight: bold;
+                gap: 7px;
+                
+
+                .bold{
+                    font-weight: bold;
+                }
+    
+                p{
+                    @include text-preview
+                }
                 .star{
                     display: flex;
+                    gap: 2px;
                     
                 }
                 .flag{
